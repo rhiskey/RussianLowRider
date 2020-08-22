ab06e9ae5bd0d7e8aa779c731f330022bbd586b2
Rhiskey <first3395@gmail.com>
2020-08-22 20:43:14 +0300

    Добавил базовый проект игры с шаблонами персонажей, эффектов, сцены, музыки и пр.

---
+++ app/src/main/java/com/example/russianlowrider/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.russianlowrider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Set No Title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Наше игровое пространство
+        this.setContentView(new GameSurface(this));
+    }
+}
